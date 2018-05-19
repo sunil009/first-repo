19ab77ee3d5af19246055821abb3cf75056adf4b
sunil akhiyniya <sunilakhiyaniya24@gmail.com>
2018-05-19 10:29:35 +0530

    check

---
+++ application/views/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test github</title>
+</head>
+<body>
+
+	<div class="tester">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+
+</body>
+</html>
